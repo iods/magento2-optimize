d06d6b1d671be47ffa43a5b495f79ac73a45caf8
Rye Miller <millerrye17@gmail.com>
2021-12-20 20:30:16 -0700

    Addition of UrlRetriever API extensions.

---
+++ Api/ProductUrlInterface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * A SEO library for getting the most out of Magento Catalog data.
+ *
+ * @author    Rye Miller <rye@drkstr.dev>
+ * @copyright Copyright (c) 2021, Rye Miller (https://ryemiller.io)
+ * @license   See LICENSE for license details.
+ */
+declare(strict_types=1);
+
+namespace Iods\Optimize\Api\Data;
+
+use Iods\Optimize\Api\UrlRetrieverInterface;
+
+/**
+ * ProductUrlInterface
+ * @package Iods\Optimize\Api\Data
+ */
+interface ProductUrlInterface extends UrlRetrieverInterface {}
