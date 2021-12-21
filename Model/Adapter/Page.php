36c81128628d061a04b93591958d68cae4814f86
Rye Miller <millerrye17@gmail.com>
2021-12-20 17:28:52 -0700

    Addition of templates for SEO blocks.

---
+++ Model/Adapter/Page.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * A SEO library for getting the most out of Magento Catalog data.
+ *
+ * @package   Iods_Optimize
+ * @author    Rye Miller <rye@drkstr.dev>
+ * @copyright Copyright (c) 2021, Rye Miller (https://ryemiller.io)
+ * @license   See LICENSE for license details.
+ */
+declare(strict_types=1);
